8e38936a11513d290d4434e4ec6deefb56a6f19f
Glitch (hello-webpage) <none>
2020-03-03 20:44:02 +0000

    🐫🍄 Checkpoint ./index.html:5252481/1116 ./style.css:5252481/567 ./script.js:5252481/3018

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+$(() => {
+  $('#image-upload').on('change', function() {
+    const file = $(this)[0].files[0];
+    const reader = new FileReader();
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+    reader.addEventListener("load", function () {
+      $('#main').css('background', `url(https://cl.ly/e6683f9832e7/gradient-01.png), url(${reader.result})`).
+                 css('background-size', 'cover');
+    }, false);
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  });
+});
