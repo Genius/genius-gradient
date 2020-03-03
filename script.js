f78d40c1188f017d25f458bacbc0d142164d14df
Glitch (hello-webpage) <none>
2020-03-03 23:53:00 +0000

    🎸🚪 Checkpoint ./script.js:5252481/2984

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ $(() => {
       $('#main').css('background', `url(https://cl.ly/e6683f9832e7/gradient-01.png), url(${reader.result})`).
                  css('background-size', 'cover');
       
+      const img = document.createElement("img");
+      img.src = 'https://cl.ly/e6683f9832e7/gradient-01.png';
+      img.onload = function() {
+        const canvas = document.createElement("canvas");
+        canvas.width = 1920;
+        canvas.height = 1080;
+        const context = canvas.getContext("2d");
+        context.globalCompositeOperation = "color";
+        context.drawImage(reader.result, 0, 0);
+        const data = canvas.toDataURL("image/jpeg");
+        callback(data);
+      };
+      
       $('#download-image').show();
     }, false);
 
@@ -24,3 +37,69 @@ $(() => {
     });
   });
 });
+
+;(function(window) {
+
+  var __f = function(obj) {
+    if (typeof window[obj] === "undefined") {
+      if (typeof console !== "undefined") {
+        console.log("Warning: canvas context not supported (" + obj + " needed)");
+      }
+      return false;
+    }
+    return true;
+  };
+
+  if (!__f("HTMLCanvasElement")) { return; }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var supportsBackgroundBlendMode = window.getComputedStyle(document.body).backgroundBlendMode;
+    if(typeof supportsBackgroundBlendMode === "undefined") {  
+
+      // TODO: check for Canvas composite support
+
+      createBlendedBackgrounds();
+    }
+  }, false);
+
+  var createBlendedBackgrounds = function() {
+    var els = document.querySelectorAll("[data-blend]");
+    for(var i = 0; i < els.length; i++) {
+      var el = els[i],
+          type = el.getAttribute("data-blend"),
+          image = el.getAttribute("data-blend-image"),
+          color = el.getAttribute("data-blend-color");
+      processElement(el,type,image,color);
+    }
+  };
+
+  var processElement = function(el,type,image,color) {
+    var backgroundImageURL = image,
+        backgroundColor = color;
+
+    createBlendedBackgroundImageFromURLAndColor(backgroundImageURL, backgroundColor, type, function(imgData) {
+      el.style.backgroundImage = "url(" + imgData + ")";
+    });
+  };
+
+  var createBlendedBackgroundImageFromURLAndColor = function(url, color, type, callback) {
+
+    // TODO: add alpha channel
+
+    var img = document.createElement("img");
+    img.src = url;
+    img.onload = function() {
+      var canvas = document.createElement("canvas");
+      canvas.width = this.naturalWidth;
+      canvas.height = this.naturalHeight;
+      var context = canvas.getContext("2d");
+      context.globalCompositeOperation = type;
+      context.drawImage(this, 0, 0);
+      context.fillStyle = color;
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      var data = canvas.toDataURL("image/jpeg");
+      callback(data);
+    };
+  };
+
+})(window);
